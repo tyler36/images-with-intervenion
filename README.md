cc42511dc99487828dc5916da3b4c3d9e5f02c50
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-02-21 12:57:35 +0900

    add example manipulations

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 ## Intro
 
 - This project demonstrations how manipulate user-supplied images
+- Use [Intervention Image](https://image.intervention.io/v2)
+
+## Demo
+
+- A user can upload an image
+- Images are automatically
+  - resized to around 1000x1000, keep aspect ratio
+  - watermark is added to bottom-right
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Models\Photo;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Intervention\Image\Facades\Image;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +25,23 @@ Route::get('/', function() {
 });
 
 Route::post('/save', function(Request $request){
-    $name = basename($request->photo->store('public'));
+    // $name = basename($request->photo->store('public'));
+
+    // Generate filename using original file extention type
+    $name = Str::random(32) . "." . $request->photo->getClientOriginalExtension();
+
+    $watermark = Image::make(storage_path('app/public/watermark.png'))
+        ->widen(400)
+        ->opacity(50);
+
+    Image::make($request->photo)
+        // Resize image, but keep aspect ration
+        ->resize(1000, 1000, function($constraint){
+            $constraint->aspectRatio();
+        })
+        // Insert watermark
+        ->insert($watermark, 'bottom-right', 20, 20 )
+        ->save(storage_path('app/public/' . $name));
 
     Photo::create([
         'name' => $name
